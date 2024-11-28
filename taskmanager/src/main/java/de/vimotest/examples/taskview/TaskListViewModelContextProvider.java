7d32052b709b624b4eddfb7b6a4f9845249b4d5e
Mario Fuksa <mario.fuksa@vector.com>
2024-11-28 10:11:15 +0100

    taskmanager: add source files

---
+++ taskmanager/src/main/java/de/vimotest/examples/taskview/TaskListViewModelContextProvider.java
@@ -0,0 +1,34 @@
+package de.vimotest.examples.taskview;
+
+import com.google.gson.Gson;
+import de.vimotest.examples.taskview.logic.Task;
+import de.vimotest.examples.taskview.logic.TaskManager;
+import de.vimotest.examples.taskview.viewmodel.TaskListViewModelImpl;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class TaskListViewModelContextProvider {
+  private String tasksJson = "";
+
+  public void Init() {
+  }
+  public TaskListViewModel BuildSut() {
+    final TaskManager taskManager = new TaskManager();
+    if (!tasksJson.isEmpty()) {
+      taskManager.addTasks(parseTasksFromJson());
+    }
+    return new TaskListViewModelImpl(taskManager);
+  }
+
+  private Collection<Task> parseTasksFromJson() {
+    Gson gson = new Gson();
+    Task[] tasks = gson.fromJson(tasksJson, Task[].class);
+    return Arrays.asList(tasks);
+  }
+
+  // example: "tasks: [ {id:\"0\", name:\"A\", prio:low, dueDate:\"2024/12/01\"} ]"
+  public void SetDataTableJson(String json) {
+    this.tasksJson = json;
+  }
+}
